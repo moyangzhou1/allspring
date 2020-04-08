04bfd3c6ba33bac56811e68ca896e129e8c7b556
15002324694zl@sina.com <zl19951025>
2020-04-08 10:59:18 +0800

    demo

--- myz-eureka/src/main/java/com/myzgroup/home/ql/eureka/MyzEurekaApp.java
+++ myz-eureka/src/main/java/com/myzgroup/home/ql/eureka/MyzEurekaApp.java
@@ -1,15 +1,10 @@
 package com.myzgroup.home.ql.eureka;
 
-import com.myzgroup.home.ql.eureka.util.LogUtil;
-import org.apache.catalina.authenticator.jaspic.AuthConfigFactoryImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.core.env.Environment;
-
-import javax.security.auth.message.config.AuthConfigFactory;
 
 /**
  * @author Administrator
@@ -24,14 +19,14 @@ public class MyzEurekaApp {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MyzEurekaApp.class);
 
-    public static void main( String[] args )
-    {
-        if (AuthConfigFactory.getFactory() == null) {
-            AuthConfigFactory.setFactory(new AuthConfigFactoryImpl());
-        }
-        Environment env  =  new SpringApplicationBuilder().sources(
-                MyzEurekaApp.class).web(true).run(args).getEnvironment();
-        LOGGER.info(LogUtil.startupLog(env));
+    public static void main( String[] args ){
+        SpringApplication.run(MyzEurekaApp.class, args);
+//        if (AuthConfigFactory.getFactory() == null) {
+//            AuthConfigFactory.setFactory(new AuthConfigFactoryImpl());
+//        }
+//        Environment env  =  new SpringApplicationBuilder().sources(
+//                MyzEurekaApp.class).web(true).run(args).getEnvironment();
+//        LOGGER.info(LogUtil.startupLog(env));
     }
 
 }
